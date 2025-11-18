889fb422241f08582aba57eba91f4394da0f6634
DanielHaas21 <daniel.haas@insio.cz>
2025-11-18 13:48:01 +0100

    Vitest tests

---
+++ src/testing/calculateFormat.test.ts
@@ -0,0 +1,68 @@
+import { describe, expect, it } from "vitest";
+import { calculateAmount } from "../utils/calculateAmount";
+
+describe("calculateAmount", () => {
+  it("calculates correct values for valid input, that is: 100k deposit and 5% interest rate with normal tax", () => {
+    const result = calculateAmount(100000, 5, 0.15);
+    expect(result).toEqual({
+      interest: 5000,
+      tax: 750,
+      total: 104250,
+    });
+  });
+});
+
+describe("calculateAmount", () => {
+  it("calculates correct values for valid input, that is: 0 deposit and 5% interest rate with normal tax", () => {
+    const result = calculateAmount(0, 5, 0.15);
+    expect(result).toEqual({
+      interest: 0,
+      tax: 0,
+      total: 0,
+    });
+  });
+});
+
+describe("calculateAmount", () => {
+  it("calculates correct values for negative numbers", () => {
+    const result = calculateAmount(-100000, 5, 0.15);
+    expect(result).toEqual({
+      interest: 0,
+      tax: 0,
+      total: 0,
+    });
+  });
+});
+
+describe("calculateAmount", () => {
+  it("calculates correct values for valid input, that is: Infite deposit and 5% interest rate with normal tax", () => {
+    const result = calculateAmount(Infinity, 5, 0.15);
+    expect(result).toEqual({
+      interest: 0,
+      tax: 0,
+      total: 0,
+    });
+  });
+});
+
+describe("calculateAmount", () => {
+  it("calculates correct values for valid input, that is: NaN deposit and 5% interest rate with normal tax", () => {
+    const result = calculateAmount(NaN, 5, 0.15);
+    expect(result).toEqual({
+      interest: 0,
+      tax: 0,
+      total: 0,
+    });
+  });
+});
+
+describe("calculateAmount", () => {
+  it("calculates correct values for valid input, that is: 0k deposit and NaN% interest rate with normal tax", () => {
+    const result = calculateAmount(10_000, NaN, 0.15);
+    expect(result).toEqual({
+      interest: 0,
+      tax: 0,
+      total: 0,
+    });
+  });
+});
--- src/utils/calculateAmount.ts
+++ src/utils/calculateAmount.ts
@@ -2,19 +2,40 @@ import type { Amount } from "../types/Amount";
 
 /**
  * Calculates interest, tax and total
- * @param amount 
- * @param ratePercent 
- * @param taxRate 
- * @returns 
+ * @param amount Initial amount colleted from input
+ * @param ratePercent fixed decimal value
+ * @param taxRate fixed decimal value
+ * @returns Object containing interest, tax and total or 0s if input is invalid
  */
 export function calculateAmount(
   amount: number,
   ratePercent: number,
   taxRate: number
 ): Amount {
+  if (amount <= 0 || ratePercent <= 0 || taxRate < 0) {
+    return {
+      interest: 0,
+      tax: 0,
+      total: 0,
+    };
+  }
   const interest = amount * (ratePercent / 100);
+
   const tax = interest * taxRate;
+
   const total = amount + interest - tax;
+
+  if (
+    !Number.isFinite(interest) ||
+    !Number.isFinite(tax) ||
+    !Number.isFinite(total)
+  ) {
+    return {
+      interest: 0,
+      tax: 0,
+      total: 0,
+    };
+  }
 
   return {
     interest: Math.round(interest),
